9ae32effb91e1143e44707b66f77d600ab56693f
NandBhanu <nandanabhanu1304@gmail.com>
2024-12-03 20:02:40 +0700

    Test pus saja

--- screen/firebase/index.js
+++ screen/firebase/index.js
@@ -1,7 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXAaPMjmyNSVO06G-scb8V88Mhiu_Jwpo",
@@ -9,14 +14,14 @@ const firebaseConfig = {
   projectId: "lelangin-e4ceb",
   storageBucket: "lelangin-e4ceb.firebasestorage.app",
   messagingSenderId: "2184481177",
-  appId: "1:2184481177:web:b8a6e3a46e8255d2ecab08"
+  appId: "1:2184481177:web:b8a6e3a46e8255d2ecab08",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
 export { db, auth, collection, addDoc, serverTimestamp };
